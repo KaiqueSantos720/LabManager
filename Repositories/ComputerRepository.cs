86abc40641a51e476c0824b5b75a541449fd1f21
Kaique Santos <e.kaique@aluno.ifsp.edu.br>
2022-05-25 07:58:25 -0300

    add connection string

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -1,14 +1,20 @@
+using LabManager.Database;
 using LabManager.Models;
 using Microsoft.Data.Sqlite;
 namespace LabManager.Repositories;
 
-class ComputerRepository
+class ComputerRepository //isolar funcionalidade de acesso a dados
 {
+    private readonly DatabaseConfig _databaseConfig;
+    public ComputerRepository(DatabaseConfig databaseConfig)
+    {
+        _databaseConfig = databaseConfig;
+    }
     public List<Computer> GetAll()
     {
         var computers = new List<Computer>();
 
-        var connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open(); //ABRIR O ARQUIVO/conexão database.db
 
 
