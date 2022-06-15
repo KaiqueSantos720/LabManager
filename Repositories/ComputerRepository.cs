aa648159c247975416cc33b06fd2d393b46e0df6
Kaique Santos <kaique.santos20042018@gmail.com>
2022-06-15 09:12:31 -0300

    Update and Delete methods refactored using Dapper

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -34,16 +34,11 @@ class ComputerRepository //isolar funcionalidade de acesso a dados
 
     public Computer Update(Computer computer)
     {
-        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        using var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
-        var command = connection.CreateCommand(); //comando criado no banco aberto
-        command.CommandText = "UPDATE Computers SET ram = ($ram), processor = ($processor) WHERE id = ($id)";
-        command.Parameters.AddWithValue("$id", computer.Id);
-        command.Parameters.AddWithValue("$ram", computer.Ram);
-        command.Parameters.AddWithValue("$processor", computer.Processor);
-        command.ExecuteNonQuery();
-        connection.Close();
+        connection.Execute("UPDATE Computers SET ram = (@Ram), processor = (@Processor) WHERE id = (@Id)", computer);
+
         return computer;
 
     }
@@ -60,20 +55,10 @@ class ComputerRepository //isolar funcionalidade de acesso a dados
 
     public void Delete(int id)
     {
-        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        using var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
-        var command = connection.CreateCommand(); //comando criado no banco aberto
-        command.CommandText = "DELETE FROM Computers WHERE id = ($id)";
-        command.Parameters.AddWithValue("$id", id);
-        command.ExecuteNonQuery();
-        connection.Close();
-    }
-
-    private Computer ReaderToComputer(SqliteDataReader reader)
-    {
-        var computer = new Computer(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-        return computer;
+        connection.Execute("DELETE FROM Computers WHERE id = (@Id)", new{Id = id});
     }
 
     public bool ExistsById(int id)
